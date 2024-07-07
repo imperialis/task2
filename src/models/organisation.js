3e2b4cef398c9409e2f68d079d3ca26e682501c5
imperialis <52557969+imperialis@users.noreply.github.com>
2024-07-07 22:34:58 +0100

    Update organisation.js

--- src/models/organisation.js
+++ src/models/organisation.js
@@ -1,23 +1,4 @@
-// const { DataTypes } = require('sequelize');
 
-// module.exports = (sequelize) => {
-//   const Organisation = sequelize.define('Organisation', {
-//     orgId: {
-//       type: DataTypes.STRING,
-//       primaryKey: true,
-//       unique: true
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     description: {
-//       type: DataTypes.STRING
-//     }
-//   });
-
-//   return Organisation;
-// };
 
 const { DataTypes } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
